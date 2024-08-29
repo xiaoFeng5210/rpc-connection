906cc0ec804e366cf1d803d569bd74558c49e414
Eadweard <aurora.coder.zqf@gmail.com>
2024-08-29 16:46:41 +0800

    玩下定时器

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from package.cli import main_cli
+import threading
+import time as Time
 import typer
 
+def testRun():
+  while True:
+    Time.sleep(1)
+    print("testRun")
+
+
 if __name__ == "__main__":
-  typer.run(main_cli)
+  # typer.run(main_cli)
+  thread = threading.Thread(target=testRun, args=())
+  thread.start()
 
