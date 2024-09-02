e11c337e73934f89aebf5de28eaa169db6205302
Eadweard <aurora.coder.zqf@gmail.com>
2024-09-02 13:46:53 +0800

    refactor: 优化代码结构

--- package/cli.py
+++ package/cli.py
@@ -7,19 +7,24 @@ current_device = ''
 current_password = ''
 
 def main_cli(device_name: str):
+    # 检查设备名称是否在设备匹配密码字典中
     if device_name in device_match_password:
-        # typer.echo(f"Hello {device_name}, {device_match_password[device_name].get('password')}")
+        # 如果设备名称在字典中，则获取密码和描述信息
+        find_obj = device_match_password[device_name]
         find_obj = device_match_password[device_name]
         password = find_obj.get("password")
         desc = find_obj.get("desc")
-        typer.echo(f"设备名称: {device_name}, 密码: {password}, 描述: {desc}")
-        global current_device, current_password
-        current_device = device_name
-        current_password = password
-        create_frp_ini(device_name)
-        run_frp_ini()
+        if password is not None:
+            typer.echo(f"设备名称: {device_name}, 密码: {password}, 描述: {desc}")
+            global current_device, current_password
+            current_device = device_name
+            current_password = password
+            create_frp_ini(device_name)
+            run_frp_ini()
+        else:
+            typer.echo(f"设备密码错误！请检查密码是否正确")
     else:
-        typer.echo(f"目前无法找到 {device_name}, ")
+        typer.echo(f"目前无法找到 {device_name}, 请检查设备名称是否正确")
 
 if __name__ == "__main__":
     typer.run(main_cli)
