ca645a36cd3498d3f0f52acad2566c9a0d040b03
Eadweard <aurora.coder.zqf@gmail.com>
2024-09-02 14:19:09 +0800

    西班牙咖啡

--- config/password.py
+++ config/password.py
@@ -10,5 +10,11 @@ device_match_password = {
     'dealer': '西班牙冰淇淋',
     'desc': '西班牙冰淇淋',
     'password': 'S9fPxP'
+  },
+
+  'qZemPx': {
+    'dealer': '西班牙咖啡',
+    'desc': '西班牙咖啡',
+    'password': 'w7bEiH'
   }
 }
